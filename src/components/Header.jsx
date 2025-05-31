43e9018b3278e150f4f66eef71f4e120cae89004
Aishwarya <pandit.aishwarya5@gmail.com>
2025-05-31 19:51:03 +0530

    routing & login feature api integration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import { signOut } from "firebase/auth";
+import React from "react";
+import { auth } from "../utils/firebase";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { removeUser } from "../redux/userSlice";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        dispatch(removeUser());
+        navigate("/");
+      })
+      .catch((error) => {
+        // An error happened.
+        console.log(error?.message);
+        navigate("/error");
+      });
+  };
   return (
-    <div className='absolute px-8 py-4 bg-gradient-to-b from-black z-10'>
-      <img className='w-45' src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="logo"/>
+    <div className="absolute px-8 py-4 flex justify-between items-center w-screen bg-gradient-to-b from-black z-10">
+      <img
+        className="w-45"
+        src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        alt="logo"
+      />
+      {auth.currentUser && (
+        <div className="">
+          <button
+            className="bg-red-700 text-white p-3 rounded-lg"
+            onClick={() => {
+              handleSignOut();
+            }}
+          >
+            Sign Out
+          </button>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,91 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { validateForm } from "../utils/validate";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { addUser } from "../redux/userSlice";
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const toggleSignInform = () => {
     setIsSignInForm(!isSignInForm);
+  };
+  const email = useRef(null);
+  const password = useRef(null);
+  const name = useRef(null);
+
+  const handleSubmit = () => {
+    const message = validateForm(email.current.value, password.current.value);
+    setErrorMessage(message);
+
+    if (!isSignInForm) {
+      //sign up
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
+          updateProfile(user, {
+            displayName: name.current.value,
+          })
+            .then(() => {
+              // Profile updated!
+              const { uid, email, displayName } = auth.currentUser;
+              dispatch(
+                addUser({ uid: uid, email: email, displayName: displayName })
+              );
+              navigate("/browse");
+              // ...
+            })
+            .catch((error) => {
+              // An error occurred
+              setErrorMessage(error.message);
+            });
+
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + " " + errorMessage);
+        });
+    } else {
+      // sign in
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          const { uid, email, displayName } = user;
+          dispatch(
+            addUser({ uid: uid, email: email, displayName: displayName })
+          );
+          navigate("/browse");
+
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + " " + errorMessage);
+        });
+    }
   };
 
   return (
@@ -17,7 +97,10 @@ const Login = () => {
           src="https://assets.nflxext.com/ffe/siteui/vlv3/af2fac72-d956-4952-8686-4d45d359d78c/web/IN-en-20250526-TRIFECTA-perspective_5db3e163-56f7-47c7-9a65-b79b9d76bf24_large.jpg"
         />
       </div>
-      <form className="w-3/12 rounded-xl bg-black/70 p-12 absolute mt-36 mx-auto text-white right-0 left-0">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-3/12 rounded-xl bg-black/70 p-12 absolute mt-36 mx-auto text-white right-0 left-0"
+      >
         <h2 className="font-bold text-3xl py-4">
           Sign {isSignInForm ? "In" : "Up"}
         </h2>
@@ -25,20 +108,27 @@ const Login = () => {
           <input
             type="text"
             placeholder="Enter Name"
+            ref={name}
             className="p-4 rounded-lg my-4 w-full bg-gray-700"
           />
         )}{" "}
         <input
           type="email"
+          ref={email}
           placeholder="Enter Email Address"
           className="p-4 rounded-lg my-4 w-full bg-gray-700"
         />
         <input
           type="password"
+          ref={password}
           placeholder="Enter Password"
           className="p-4 rounded-lg my-4 w-full  bg-gray-700"
         />
-        <button className="p-4 my-4 bg-red-800 w-full rounded-lg">
+        <p className="text-red-800 font-bold text-lg">{errorMessage}</p>
+        <button
+          className="p-4 my-4 bg-red-800 w-full rounded-lg"
+          onClick={handleSubmit}
+        >
           Sign {isSignInForm ? "In" : "Up"}
         </button>
         <p onClick={() => toggleSignInform()} className="py-6 cursor-pointer">
